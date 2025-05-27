a9fe7a4df8201d17d37a6aea90ce72bd0a1a1bf8
Palak Sharma <122739109+palaksharma99@users.noreply.github.com>
2025-05-27 22:20:18 +0530

    Add files via upload

---
+++ CALENDAR3.cpp
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<conio.h>
+    int getmonth(int,int);
+    int main()
+	
+	{   
+                  int yr, x, y, z, mon;
+                  
+                  printf("Enter an year to show the calendar :" );
+                  scanf("%d",&yr);
+                  x = ((yr-1)%400) / 100;
+                  y = (((yr-1)%400) % 100) / 4;
+                  z = (((yr-1)%400) % 100) - (((yr-1)%400) % 100) / 4;
+                  mon = (x*5+y*2+z+1) % 7;
+                  
+                  printf("\n\n\n\nJanuary\n\n");
+                  mon=getmonth(31,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                  
+                  printf("\n\n\n\nFebruary\n\n");
+                  if((yr)%4==0)
+                  { if(mon=getmonth(29,mon))
+                   printf("\n\nIT'S A LEAP YEAR");}
+                  else
+                    mon=getmonth(28,mon);
+                    
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                  
+                  printf("\n\n\n\nMarch\n\n");
+                  mon=getmonth(31,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                  
+                  printf("\n\n\n\nApril\n\n");
+                  mon=getmonth(30,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                  
+                  printf("\n\n\n\nMay\n\n");
+                  mon=getmonth(31,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                   
+                  printf("\n\n\n\nJune\n\n");
+                  mon=getmonth(30,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                   
+                  printf("\n\n\n\nJuly\n\n");
+                  mon=getmonth(31,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                   
+                  printf("\n\n\n\nAugust\n\n");
+                  mon=getmonth(31,mon);
+                  printf("\n\n\n Press any key to display next month . . .");
+                  getche();
+                   
+                   printf("\n\n\n\nSeptember\n\n");
+                   mon=getmonth(30,mon);
+                   printf("\n\n\n Press any key to display next month . . .");
+                   getche();
+                    
+                   printf("\n\n\n\nOctober\n\n");
+                   mon=getmonth(31,mon);
+                   printf("\n\n\n Press any key to display next month . . .");
+                   getche();
+                    
+                   printf("\n\n\n\nNovember\n\n");
+                   mon=getmonth(30,mon);
+                   printf("\n\n\n Press any key to display next month . . .");
+                   getche();
+                    
+                   printf("\n\n\n\nDecember\n\n");
+                   mon=getmonth(31,mon);
+     getch();
+     }
+     int getmonth(int p,int q)
+     {
+                   int a,b,c;
+                   printf("\nSun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+                   for(a=0;a<=q;a++)
+                   if(a>=q)
+                   {
+                   for(b=1;b<=p;b++)
+                   {
+                    if((a % 7) == 0)
+                    printf("\n");
+                    printf("%8d",b);
+                    a++;
+                    }
+                    }
+                    else
+                    printf("\t");
+                    c=(p+q)%7;
+    return(c);
+    }
